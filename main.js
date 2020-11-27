76ac000265c0665d6d56a2fae17ea1f29fdddd46
salvoab <salvatore.abate85@gmail.com>
2020-11-27 15:57:53 +0100

    Implementato Event Listener per click su div.prev
    Utilizzata la stessa logica dell'event listener del next, riadattata per il div.prev

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ $(function(){
     $('.next').click(function () { 
         var activeImage = $('.images').find('.active');
         activeImage.removeClass('active');
-        
+        //Se l'attuale immagine attiva è l'ultima allora aggiungo la classe active al primo
+        //Altrimenti metto la classe active all'elemento successivo
         if(activeImage.hasClass('last')){
             $('.first').addClass('active');
         } else{
             activeImage.next().addClass('active');
         }
 
+        //stessa logica utilizzata sopra per le immagini
         var activeCircle = $('.nav').find('.active');
         activeCircle.removeClass('active');
         
@@ -17,6 +19,29 @@ $(function(){
             $('.first').addClass('active');
         } else{
             activeCircle.next().addClass('active');
+        }
+    });
+
+    // Event Listener del click sul div di classe prev
+    $('.prev').click(function () { 
+        var activeImage = $('.images').find('.active');
+        activeImage.removeClass('active');
+        //Se l'attuale immagine attiva è la prima allora aggiungo la classe active all'ultima
+        //Altrimenti metto la classe active all'elemento precedente
+        if(activeImage.hasClass('first')){
+            $('.last').addClass('active');
+        } else{
+            activeImage.prev().addClass('active');
+        }
+
+        //stessa logica utilizzata sopra per le immagini
+        var activeCircle = $('.nav').find('.active');
+        activeCircle.removeClass('active');
+        
+        if(activeCircle.hasClass('first')){
+            $('.last').addClass('active');
+        } else{
+            activeCircle.prev().addClass('active');
         }
     });
 });
